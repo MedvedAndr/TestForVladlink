6b10f8530adb7a1fe9500fa7a9ebd12d0023bbc6
MedvedAndr <158015192+MedvedAndr@users.noreply.github.com>
2024-02-02 15:49:42 +1000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,8 @@
 <?php
 function connection()
 {
+	global $db;
+	
 	$connection = new mysqli($db['host'], $db['login'], $db['pass']);
 	$connection->select_db($db['db_name']);
 	$connection->query('SET NAMES utf8');
